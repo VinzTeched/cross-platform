a58a443f4cdc0e56a00882e3bc1f5b932c384542
Chibundo Ilonze <chibundoilonze@gmail.com>
2022-11-18 08:20:42 +0100

    commit

---
+++ react/customizing-example/src/components/Intro3.js
@@ -0,0 +1,13 @@
+function Intro3() {
+    return (
+        <div classNamee="blog-post-intro">
+            <h2>What is the best way to style your React app?</h2>
+            <div>
+                <p>There are so many options to choose from. Here's a high level overview of the popular ones.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Intro3;
+
