9f7443c121e94ba069c23d7224e77471c37ce56c
Chibundo Ilonze <chibundoilonze@gmail.com>
2022-11-26 06:42:10 +0100

    new commit

--- react/my-video/src/App.js
+++ react/my-video/src/App.js
@@ -4,18 +4,19 @@ import ReactPlayer from "react-player"
 
 function App() {
 
-  const videoUrl = 'https://www.youtube.com/watch?v=ysz5S6PUM-U';
+  const videoUrl = 'https://www.facebook.com/facebook/videos/10153231379946729/';
 
   return (
       <div className="App">
         <MyVideo />
+        <ReactPlayer url = {videoUrl} playing={false} volume={0.5} />
       </div>
   );
 };
 
 const MyVideo = () => {
   return (
-    <ReactPlayer url={videoUrl} playing={false} volume={0.5} />
+    <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
   );
 };
 
