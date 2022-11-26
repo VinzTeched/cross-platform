cfd7100955c54edd0c99f409d5264c31b084d9e6
Chibundo Ilonze <chibundoilonze@gmail.com>
2022-11-26 06:04:34 +0100

    new commit

---
+++ react/my-video/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ShowAnimal from "./ShowAnimal";
+
+function App() {
+
+
+  return (
+      <div className="App">
+        <ShowAnimal />
+      </div>
+  );
+};
+
+export default App;
