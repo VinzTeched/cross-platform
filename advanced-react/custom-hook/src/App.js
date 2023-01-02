6b2819d9fa85de7269fa2f7bd7476c79744f3ab8
Chibundo Ilonze <chibundoilonze@gmail.com>
2023-01-03 00:22:15 +0100

    Add commits

---
+++ advanced-react/custom-hook/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CreateCustomHook from './CreateCustomHook';
+import One from './One';
+
+function App() {
+
+  return (
+    <div className="App">
+      <CreateCustomHook />
+    </div>
+  );
+}
+
+export default App;
